63b340bf9224c3b81684fa30d486d2caf4fa1b5e
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-15 13:52:22 +0900

    todo機能実装

--- src/app.js
+++ src/app.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styles from './css/app.css';
+import Todo from './todos';
 
-export default class App extends React.Component {
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            input: "",
+            todos: [],
+            time: new Date(),
+            intervalId: ""
+        }
+        this.addToDo = this.addToDo.bind(this)
+        this.tick = this.tick.bind(this)
+        this.removeToDo = this.removeToDo.bind(this)
+    }
+
+    addToDo() {
+        let { todos, input } = this.state
+        todos = todos.concat(input)
+        this.setState({todos: todos, input: ""})
+    }
+
+    tick() {
+        let { time } = this.state
+        time.setSeconds(time.getSeconds() + 1)
+        this.setState({time: time})
+    }
+
+    removeToDo(i) {
+        let { todos } = this.state
+        todos = todos.slice(0, i).concat(todos.slice(i + 1))
+        this.setState({todos: todos, input: ""})
+    }
+
+    componentDidMount() {
+        let id = setInterval(this.tick, 1000)
+        this.setState({intervalId: id})
+    }
+    componentWillUnmount() {
+        clearInterval(this.state.intervalId)
+    }
+
     render() {
         return (
-            <div id="main">
-                <h1 className="title">{"Hello World"}</h1>
+            <div>
+                <h1>新しいタスク</h1>
+                <input type="text" onChange={e => this.setState({input: e.target.value})} value={this.state.input} />
+                <button onClick={this.addToDo}>追加</button>
+                <h2>現在の時間</h2>
+                <span>今の時間 {this.state.time.toLocaleTimeString()}</span>
+                <h2>ToDo:</h2>
+                <Todo todos={this.state.todos} removeToDo={this.removeToDo} />
             </div>
         )
     }
